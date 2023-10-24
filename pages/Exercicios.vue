96efdc3b89dee656c6c4ac05dbc1cb0838b09726
Davi <davi.frrs@gmail.com>
2023-10-24 17:04:32 -0300

    feat: initial commit

---
+++ pages/Exercicios.vue
@@ -0,0 +1,28 @@
+
+
+<script setup>
+import axios from "axios";
+const data = ref([]);
+const tableColumns = [{
+    key: 'id',
+    label: 'Código'
+}, {
+    key: 'nome',
+    label: 'Nome'
+}, {
+    key: 'imagem',
+    label: 'Imagem'
+}]
+
+const res = await axios.get("http://localhost:5097/api/Exercicios");
+data.value = res.data;
+
+</script>
+<template>
+    <main>
+        <div class="bg-green-300 p-10 text-2xl font-semibold">
+            <h1>Exercícios</h1>
+        </div>
+        <UTable :columns="tableColumns" :rows="data" />
+    </main>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,8 @@
+<template>
+    <div class="bg-green-300 p-10">
+        Olá mundo
+        <div id="badge" class="w-16 text-center rounded-2xl shadow bg-yellow-300 font-bold text-gray-900">
+            new
+        </div>
+    </div>
+</template>
